1bcb0f080fdaeb1e8cadc96b237b86338abee236
theluqmn <luqman.yazrie@outlook.com>
2025-08-25 19:25:51 +0800

    add position procedure, and code to add procedure but doesnt write to the file

--- src/main.cbl
+++ src/main.cbl
@@ -15,35 +15,69 @@
        FILE SECTION.
        FD POSITION-FILE.
        01 POSITION-RECORD.
-           05 POSITION-ID      PIC X(10).
-           05 POSITION-NAME    PIC X(20).
-           05 BASE-SALARY      PIC 9(10).
+           05 POSITION-ID PIC X(10).
+           05 POSITION-NAME PIC X(20).
+           05 POSITION-SALARY PIC 9(10).
        WORKING-STORAGE SECTION.
        01 CLI-INPUT PIC X(32).
        01 FS-POSITION PIC XX.
 
        PROCEDURE DIVISION.
+       DISPLAY "CRUNCH - human resource management done right".
+       DISPLAY " ".
+       DISPLAY "run 'help' for list of available commands.".
+       DISPLAY "run 'setup' if this is the first time.".
        PERFORM PROCEDURE-MAIN.
        CLI-HANDLER.
-           DISPLAY "------------------------------------------".
+           DISPLAY "---------------------------------------------".
            DISPLAY "> " WITH NO ADVANCING.
            ACCEPT CLI-INPUT.
            IF CLI-INPUT = "setup" THEN
                PERFORM PROCEDURE-SETUP
+           ELSE IF CLI-INPUT = "position" THEN
+               PERFORM PROCEDURE-POSITION
+           ELSE IF CLI-INPUT = "position-add" THEN
+               PERFORM POSITION-ADD
+           ELSE IF CLI-INPUT = "exit" THEN
+               DISPLAY "exiting..."
            ELSE
                DISPLAY "unknown command entered"
            END-IF.
        PROCEDURE-SETUP.
-           DISPLAY "------------------------------------------".
-           DISPLAY "[ SETUP CRUNCH ]".
+           DISPLAY "---------------------------------------------".
+           DISPLAY "SETUP CRUNCH".
            DISPLAY " ".
+
            OPEN OUTPUT POSITION-FILE.
            CLOSE POSITION-FILE.
+
            DISPLAY "(1/1) position file created".
            DISPLAY "setup complete".
+       PROCEDURE-POSITION.
+           DISPLAY "---------------------------------------------".
+           DISPLAY "POSITION OVERVIEW".
+           DISPLAY " ".
+           DISPLAY "[position-add]     add a new position.".
+
+           POSITION-ADD.
+           DISPLAY "---------------------------------------------".
+           DISPLAY "ADD A NEW POSITION".
+           DISPLAY " ".
+           DISPLAY "(1/3) id:          " WITH NO ADVANCING.
+           ACCEPT POSITION-ID.
+           DISPLAY "(2/3) name:        " WITH NO ADVANCING.
+           ACCEPT POSITION-NAME.
+           DISPLAY "(3/3) base salary: " WITH NO ADVANCING.
+           ACCEPT POSITION-SALARY.
+
+           OPEN I-O POSITION-FILE.
+           WRITE POSITION-RECORD.
+           CLOSE POSITION-FILE.
+
+           DISPLAY " ".
+           DISPLAY "position added successfully.".
        PROCEDURE-MAIN.
            PERFORM CLI-HANDLER UNTIL CLI-INPUT = "exit".
-           DISPLAY "exiting crunch...".
            STOP RUN.
 
        END PROGRAM CRUNCH.
